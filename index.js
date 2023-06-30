75a53971ce4f480f8939655af2fcb716af2400f5
WellingtonGabriel1 <WellingtonGabriel7700@gmail.com>
2023-06-30 08:12:29 -0300

    feat: integrate user initial validation

---
+++ index.js
@@ -0,0 +1,27 @@
+const validateUser = async (email) => {
+    try {
+        const result = await fetch(`https://mp-wallet-app-api.herokuapp.com/users?email=${email}`);
+        const user = await result.json();
+        return user;
+    } catch (error) {
+        return { error };
+    }
+ };
+
+const onClickLogin = async () => {
+    const email = document.getElementById("input-email").value;
+    if (email.length < 5 || !email.includes("@")) {
+        alert("Email invalido!");
+        return;
+    }
+
+    const result = await validateUser(email);
+    if(result.error) {
+        alert('Falha ao validar e-mail')
+        return;
+    }
+
+    localStorage.setItem("@WalletApp:userEmail", email);
+    window.open("./pages/home/index.html", "_self");
+};
+
